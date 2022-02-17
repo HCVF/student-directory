125ee0f8ee53b279873fe391e8d9adc5fe5a5554
Henry <hcvferrari@gmail.com>
2022-02-17 17:55:39 +0000

    Ex.7: default cohortvalue set, converts to sym, measures against typos

--- exercises.rb
+++ exercises.rb
@@ -11,8 +11,39 @@ def input_students
     puts "Height of student in cm please".center(50)
     # get height
     height = gets.chomp.chomp("cm")
+    # get cohort, if spelling mistake is made but not in the first 3 letters, the month will always register the correct symbol
+    puts "Enter sudent's cohort".center(50) 
+    cohort = gets.chomp.capitalize
+    if cohort.empty? || cohort.include?("Ja")
+      cohort = :January
+    elsif cohort.include?("F")
+      cohort = :February
+    elsif cohort.include?("Mar")
+      cohort = :March
+    elsif cohort.include?("Ap")
+      cohort = :April
+    elsif cohort.include?("May")
+      cohort = :May
+    elsif cohort.include?("Jun")
+      cohort = :June
+    elsif cohort.include?("Jul")
+      cohort = :July
+    elsif cohort.include?("Au")
+      cohort = :August
+    elsif cohort.include?("S")
+      cohort = :September
+    elsif cohort.include?("O")
+      cohort = :October
+    elsif cohort.include?("N")
+      cohort = :November
+    elsif cohort.include?("D")
+      cohort = :December
+    else
+      puts "Please carefully enter a month (e.g. 'February')".center(50)
+      cohort = gets.chomp.capitalize.to_sym
+    end 
     # add the student hash to the array
-    students << {name: name, cohort: :november, height: height}
+    students << {name: name, cohort: cohort, height: height}
     puts "Now we have #{students.count} students\n\n".center(50)
     # get another name from the user, ask for name again for clarity
     puts "Name of next student please".center(50)
